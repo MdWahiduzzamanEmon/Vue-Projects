1839d29803b20fee5a4d783adc7f11bb73ab78ef
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-03-22 14:31:37 +0600

    notes done

--- Notes/src/components/Home/Notes/NoteS.vue
+++ Notes/src/components/Home/Notes/NoteS.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <header class="notes_header">
-      <h1>Notes</h1>
+      <h1>Notes {{ notes.length > 0 ? -notes.length : '' }}</h1>
       <!-- <p>Notes is a simple note taking app.</p> -->
       <button @click="showOverlay">+</button>
     </header>
@@ -9,46 +9,174 @@
     <section class="overlay" v-if="overlay">
       <div class="overlay_modal">
         <h2>Add Note</h2>
-        <form>
+        <form @submit.prevent="addNote">
           <label for="title">Title</label>
-          <input type="text" name="title" id="title" />
+          <input type="text" name="title" id="title" v-model.trim="data.title" />
 
           <label for="description">Description</label>
-          <textarea name="description" id="description" cols="30" rows="10"></textarea>
+          <textarea
+            name="description"
+            id="description"
+            cols="30"
+            rows="10"
+            v-model.trim="data.description"
+          />
 
           <label for="category">Category</label>
-          <select name="category" id="category">
+          <select name="category" id="category" v-model="data.category">
             <option value="work">Work</option>
             <option value="personal">Personal</option>
             <option value="shopping">Shopping</option>
             <option value="others">Others</option>
           </select>
-
-          <button type="submit">Add Note</button>
+          <small style="color: red">{{ errorMessage }}</small>
+          <button type="submit" v-if="!noteEdit">Add Note</button>
+          <button type="submit" @click="editNote" v-if="noteEdit">Edit Note</button>
           <button type="button" @click="hideOverlay">Cancel</button>
         </form>
       </div>
     </section>
 
     <!-- notes section  -->
-    
+    <section class="notes">
+      <div
+        class="note"
+        v-for="note in notes"
+        :key="note.id"
+        :style="{
+          backgroundColor: note.backgroundColor
+        }"
+      >
+        <h2>{{ note.title }}</h2>
+        <p>
+          {{ note.description }}
+        </p>
+        <label>
+          Created on:
+          <time>
+            {{ note.date }}
+          </time>
+        </label>
+        <div class="note_footer">
+          <small>Category: {{ note.category }}</small>
+
+          <div>
+            <button @click="editModal(note)">Edit</button>
+            <button @click="deleteNote(note)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
+// script setup
 <script setup>
 import { ref } from 'vue'
 
+//states
 const overlay = ref(false)
+const notes = ref([])
+const data = ref({
+  title: '',
+  description: '',
+  category: ''
+})
+const errorMessage = ref('')
+const noteEdit = ref(false)
 
+//show modal
 const showOverlay = () => {
   overlay.value = true
 }
 
+//hide modal
 const hideOverlay = () => {
   overlay.value = false
+  errorMessage.value = ''
+  data.value.title = ''
+  data.value.description = ''
+  data.value.category = ''
+  editModal.value = false
+  noteEdit.value = false
+}
+
+//random color
+const getRandomColor = () => {
+  ///maker rgba color
+  return `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(
+    Math.random() * 255
+  )}, 0.5)`
+}
+
+//add new note
+const addNote = () => {
+  noteEdit.value = false
+  if (data.value.title === '' || data.value.description === '' || data.value.category === '') {
+    return (errorMessage.value = 'Please fill all the fields')
+  } else {
+    errorMessage.value = ''
+  }
+
+  notes.value.push({
+    id: Math.floor(Math.random() * 100000000),
+    title: data.value.title,
+    description: data.value.description,
+    category: data.value.category,
+    date: new Date().toISOString().split('T')[0],
+    backgroundColor: getRandomColor()
+  })
+  hideOverlay()
+  data.value.title = ''
+  data.value.description = ''
+  data.value.category = ''
+  errorMessage.value = ''
+}
+
+//note edit modal
+const editModal = (note) => {
+  overlay.value = true
+  noteEdit.value = true
+  // console.log(note)
+  data.value.title = note.title
+  data.value.description = note.description
+  data.value.category = note.category
+}
+
+//note edit
+const editNote = () => {
+  if (data.value.title === '' || data.value.description === '' || data.value.category === '') {
+    return (errorMessage.value = 'Please fill all the fields')
+  } else {
+    errorMessage.value = ''
+  }
+  if (notes.value.length > 0) {
+    notes.value = notes.value.filter((note) => note.title !== data.value.title)
+
+    notes.value.push({
+      id: Math.floor(Math.random() * 100000000),
+      title: data.value.title,
+      description: data.value.description,
+      category: data.value.category,
+      date: new Date().toISOString().split('T')[0],
+      backgroundColor: getRandomColor()
+    })
+  }
+  hideOverlay()
+  data.value.title = ''
+  data.value.description = ''
+  data.value.category = ''
+  errorMessage.value = ''
+}
+
+//note delete
+const deleteNote = (note) => {
+  notes.value = notes.value.filter((n) => n.id !== note.id) // this is the best way to delete an item from an array
+  // notes.value.splice(notes.value.indexOf(note), 1)// this is the second best way to delete an item from an array
 }
 </script>
 
+// css file
 <style scoped>
 .notes_header {
   display: flex;
@@ -168,5 +296,120 @@ const hideOverlay = () => {
 
 .overlay_modal form button[type='submit']:focus {
   outline: none;
+}
+
+/* // notes section */
+
+.notes {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 1rem;
+  padding: 1rem;
+}
+
+.note {
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 1rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.note h2 {
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin: 0;
+}
+
+.note p {
+  font-size: 1rem;
+  font-weight: 400;
+  margin: 0;
+}
+
+.note label {
+  font-size: 0.7rem;
+  font-weight: 600;
+  margin: 0;
+}
+
+.note .note_footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 1rem;
+}
+
+.note .note_footer small {
+  font-size: 1rem;
+  font-weight: 400;
+  color: #747373;
+}
+
+.note .note_footer button:not(:last-child) {
+  margin-right: 1rem;
+  background-color: green;
+}
+.note .note_footer button:not(:first-child) {
+  margin-right: 1rem;
+  background-color: red;
+}
+
+.note .note_footer button {
+  padding: 0.5rem;
+  border: none;
+  border-radius: 5px;
+  background-color: black;
+  color: #fff;
+  cursor: pointer;
+}
+
+.note .note_footer button:hover {
+  opacity: 0.9;
+}
+
+.note .note_footer button:focus {
+  outline: none;
+}
+
+@media (max-width: 768px) {
+  .notes {
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  }
+}
+
+@media (max-width: 576px) {
+  .notes {
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  }
+}
+
+@media (max-width: 400px) {
+  .notes {
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  }
+}
+
+@media (max-width: 300px) {
+  .notes {
+    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  }
+}
+
+@media (max-width: 200px) {
+  .notes {
+    grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
+  }
+}
+
+@media (max-width: 100px) {
+  .notes {
+    grid-template-columns: repeat(auto-fit, minmax(25px, 1fr));
+  }
+}
+
+@media (max-width: 50px) {
+  .notes {
+    grid-template-columns: repeat(auto-fit, minmax(10px, 1fr));
+  }
 }
 </style>
