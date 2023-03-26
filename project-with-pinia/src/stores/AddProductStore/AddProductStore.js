c2c98fffb2f5c856066ab4c2015d43794fa3df43
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-03-26 23:13:58 +0600

    add

--- project-with-pinia/src/stores/AddProductStore/AddProductStore.js
+++ project-with-pinia/src/stores/AddProductStore/AddProductStore.js
@@ -2,24 +2,23 @@ import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 import NotifyToast from '@/Utility/NotifyToast/NotifyToast.js'
 
-
 const useAddProductStore = defineStore('addProduct', () => {
+  //state for add product
   const addProductList = ref([])
   const detailsSingleProduct = ref({})
 
-  // console.log(detailsSingleProduct.value)
-
+  //getter for total price
   const totalPrice = computed(() => {
     return addProductList.value.reduce((total, product) => {
       return Math.round((total + product.price * product.quantity) * 100) / 100
     }, 0)
   })
 
+  //actions for add product
   const addProduct = (product) => {
     //also add same product select quantity
     const index = addProductList.value.findIndex((item) => item.id === product.id)
     if (index !== -1) {
-      
       NotifyToast('Product already in cart', 'error')
     } else {
       addProductList.value.push({
@@ -30,46 +29,47 @@ const useAddProductStore = defineStore('addProduct', () => {
     }
   }
 
+  //  add single product
   const addSingleProduct = (product) => {
     // if product already in cart show alert
     const index = addProductList.value.findIndex((item) => item.id === product.id)
     if (index !== -1) {
       NotifyToast('Product already in cart', 'error')
-    }
-    else {
+    } else {
       addProductList.value.push({
         ...product
       })
       NotifyToast('Product added to cart', 'success')
     }
   }
-  //   addition of price of same product
 
+  //  addition of price of same product
   const increaseQuantity = (product) => {
     // console.log(product)
-    if(addProductList.value.length >0 ){
-    const index = addProductList.value.findIndex((item) => item.id === product.id)
-    addProductList.value[index].quantity += 1
+    if (addProductList.value.length > 0) {
+      const index = addProductList.value.findIndex((item) => item.id === product.id)
+      addProductList.value[index].quantity += 1
     }
-    
+
     detailsSingleProduct.value.quantity += 1
   }
 
+  //  subtraction of price of same product
   const decreaseQuantity = (prodcut) => {
-    if(addProductList.value.length >0 ){
-    const index = addProductList.value.findIndex((item) => item.id === prodcut.id)
-    if (addProductList.value[index].quantity > 1) {
-      addProductList.value[index].quantity -= 1
-    } else {
-      addProductList.value.splice(index, 1)
+    if (addProductList.value.length > 0) {
+      const index = addProductList.value.findIndex((item) => item.id === prodcut.id)
+      if (addProductList.value[index].quantity > 1) {
+        addProductList.value[index].quantity -= 1
+      } else {
+        addProductList.value.splice(index, 1)
+      }
     }
-  }
     if (detailsSingleProduct.value.quantity > 1) {
       detailsSingleProduct.value.quantity -= 1
     }
-    
   }
 
+  //  remove product from cart
   const removeProduct = (index) => {
     addProductList.value.splice(index, 1)
   }
--- project-with-pinia/src/views/Home/ProductDetails/DetailsProduct.vue
+++ project-with-pinia/src/views/Home/ProductDetails/DetailsProduct.vue
@@ -1,6 +1,6 @@
 <template>
+  <!-- //back button  -->
   <section class="d-flex justify-content-end align-items-center">
-    <!-- //back button  -->
     <router-link to="/">
       <button class="btn btn-outline-primary">
         <svg
@@ -18,19 +18,21 @@
       </button>
     </router-link>
   </section>
+
   <!-- loading  -->
   <div v-if="loading" class="d-flex justify-content-center align-items-center">
     <div class="spinner-grow" role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
   </div>
+
   <!-- error  -->
   <div v-if="error" class="d-flex justify-content-center align-items-center">
     <h1>{{ error }}</h1>
   </div>
 
   <!-- main details  -->
-  <main v-if="detailsSingleProduct" class="container">
+  <main v-if="detailsSingleProduct && !loading" class="container">
     <div class="row align-items-center">
       <div class="col-md-6 d-flex justify-content-center align-items-center">
         <img
@@ -69,9 +71,40 @@
           </p>
         </div>
 
-        <button class="btn btn-primary" @click="addSingleProduct(detailsSingleProduct)">
-          Add to cart
-        </button>
+        <!-- // buy now and add to cart button -->
+        <section class="d-flex justify-content-end gap-2">
+          <!-- //buy now button -->
+          <button class="btn btn-success d-flex align-items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-bag-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"
+              />
+            </svg>
+            Buy Now
+          </button>
+          <!-- // add to cart button -->
+          <button class="btn btn-primary" @click="addSingleProduct(detailsSingleProduct)">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-cart-plus-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"
+              />
+            </svg>
+          </button>
+        </section>
       </div>
     </div>
   </main>
@@ -82,24 +115,24 @@ import { storeToRefs } from 'pinia'
 import { onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useProductStore } from '../../../stores/ProductStore/ProductStore'
-
 import useAddProductStore from '../../../stores/AddProductStore/AddProductStore'
 
+//get id from route
 const router = useRoute()
 const id = router.params.id
 
+//all product store
 const productStore = useProductStore()
 const { loading, error } = storeToRefs(productStore)
 const { singleProduct } = productStore
 
+//add product store
 const addProductStore = useAddProductStore()
 const { increaseQuantity, decreaseQuantity, addSingleProduct } = addProductStore
 const { detailsSingleProduct } = storeToRefs(addProductStore)
 
-// console.log(detailsSingleProduct.value)
+//mounted hook to fetch single product
 onMounted(() => {
   singleProduct(id)
 })
 </script>
-
-<style scoped></style>
